33b6741e1cb12cd149b9e1d9f0f4cb13abcefb9e
lucas <lucas.lopesdf@gmail.com>
2020-01-08 23:34:16 -0300

    Lista e Grid views dinamicos.

--- lib/pages/hello_listview.dart
+++ lib/pages/hello_listview.dart
@@ -1,4 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+class Dog {
+  String nome;
+  String foto;
+
+  Dog(this.nome, this.foto);
+}
 
 class HelloListView extends StatelessWidget {
   @override
@@ -6,6 +14,10 @@ class HelloListView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("ListView"),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: (){},),
+          IconButton(icon: Icon(Icons.grid_on), onPressed: (){},),
+        ],
       ),
       body: _body(),
 
@@ -13,17 +25,76 @@ class HelloListView extends StatelessWidget {
   }
 
   _body() {
-    return ListView(
-      itemExtent: 350,
-      children: <Widget>[
-        _img("assets/images/dog1.jpg"),
-        _img("assets/images/dog2.jpeg"),
-        _img("assets/images/dog3.jpg"),
-        _img("assets/images/dog4.jpg"),
-        _img("assets/images/dog5.jpg"),
-      ],
+
+    List<Dog> dogs = [
+      Dog("Fluidao","assets/images/dog1.jpg"),
+      Dog("Rapadura","assets/images/dog2.jpeg"),
+      Dog("Mosquito","assets/images/dog3.jpg"),
+      Dog("Bob","assets/images/dog4.jpg"),
+      Dog("Flash","assets/images/dog5.jpg"),
+    ];
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemCount: dogs.length,
+      itemBuilder: (context, index) {
+        Dog d = dogs[index];
+        return Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            _img(d.foto),
+            Align (
+              alignment: Alignment.bottomRight,
+              child: Container(
+                margin: EdgeInsets.all(12),
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Text(d.nome, style: TextStyle(
+                  color: Colors.pink,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),),
+              ),
+            )
+          ],
+        );
+      },
     );
   }
+
+
+//    return ListView.builder(
+//      itemCount: dogs.length,
+//      itemExtent: 350,
+//      itemBuilder: (context, index) {
+//        Dog d = dogs[index];
+//        return Stack(
+//          fit: StackFit.expand,
+//          children: <Widget>[
+//            _img(d.foto),
+//            Align (
+//              alignment: Alignment.bottomRight,
+//              child: Container(
+//                margin: EdgeInsets.all(12),
+//                padding: EdgeInsets.all(12),
+//                decoration: BoxDecoration(
+//                  color: Colors.yellow,
+//                  borderRadius: BorderRadius.circular(16),
+//                ),
+//                child: Text(d.nome, style: TextStyle(
+//                  color: Colors.pink,
+//                  fontSize: 30,
+//                  fontWeight: FontWeight.bold,
+//                ),),
+//              ),
+//            )
+//          ],
+//        );
+//      },
+//    );
+//  }
 
   _img(source) {
 //    return Image.network("https://abrilvejario.files.wordpress.com/2016/12/3659_cachorro2.jpeg");
